48cc2ee247bd27406a0513fa9fb5d90ced51ad2e
muhammadqodir.a <azamovm323@gmail.com>
2024-11-11 20:18:54 +0500

    Initial

---
+++ Dictionary/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dictionary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Dictionary/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dictionary
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
